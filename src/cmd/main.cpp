8778a737a4d9408864ff196b82f2a6aed300737f
loader3229 <1010903229@qq.com>
2025-10-14 16:25:51 +0800

    commit

--- src/cmd/main.cpp
+++ src/cmd/main.cpp
@@ -3,6 +3,7 @@
 #include <thread>
 
 #include "cmd/commands.hpp"
+#include "cmd/test.hpp"
 #include "mine/api_client.hpp"
 #include "sys/file.hpp"
 #include "sys/log.hpp"
@@ -240,6 +241,9 @@ int dispatch(Settings settings, const std::vector<std::string>& args) {
       id = args.at(1);
     }
     commands.testRange(id);
+  } else if (cmd == "test-minimizer") {
+    Test test;
+    test.minimizer(10000);
   } else if (cmd == "generate" || cmd == "gen") {
     commands.generate();
   } else if (cmd == "migrate") {
