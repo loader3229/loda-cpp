68f600150698dcc3dc3b3e0e3726ba272fd4e752
loader3229 <1010903229@qq.com>
2026-01-06 20:33:06 +0800

    commit

--- src/compile_commands.py
+++ src/compile_commands.py
@@ -1,13 +1,13 @@
 # Generate compile_commands.json
 
-cmd = "g++ -I. -Id:/loda-cpp/installed/x64-windows/include -g -Wall -Werror -fmessage-length=0 -std=c++17 -O2 -c"
+cmd = "g++ -I. -Id:/loda-cpp/installed/x64-windows/include -D_CRT_SECURE_NO_WARNINGS -g -Wall -Werror -fmessage-length=0 -std=c++17 -O2 -c"
 import os
 compile_commands = open("compile_commands.json", "w")
 compile_commands.write("[")
 a = os.listdir("d:/loda-cpp/src")
 flag = False
 for i in a:
-  if i.count(".") == 0 and i.count("Makefile") == 0:
+  if i.count(".") == 0 and i.count("Makefile") == 0 and i != "loda":
     b = os.listdir("d:/loda-cpp/src/" + i)
     for j in b:
       if j.count(".cpp") == 1:
